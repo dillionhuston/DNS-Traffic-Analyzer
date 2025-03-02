416296767f5f35c62e4964d3e6fe8a064698ec04
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-03-02 14:42:43 +0000

    Basic dns packet logger that stores into json
    Just a quick DNS logger needed to basic analysis. in the future this will become better

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ from scapy.all import sniff, DNS, DNSQR, packet
 #import csv libary 
 import pandas as pd
 import numpy as np 
+import json
+import pickle
 import csv
 
 def process_pack(packet):
     if packet.haslayer(DNS) and packet.haslayer(DNSQR):
 
-        dns_query = {
-            "timestamp": packet.time,
-            "source": packet[1].src,
-            "destination": packet[1].dst,
-            "query_name": packet[DNSQR].qname.decode(),
-            "query-type": packet[DNSQR].qtype
-        }
-        dns_quries.append(dns_query)
+        dns_query = {}
+        
+    
+        dns_query["timestamp"] = packet.time
+        dns_query["src_ip"] = packet[1].src
+        dns_query["dst_ip"] = packet[1].dst
+        dns_query["query_name"] = packet[DNSQR].qname.decode()
+        dns_query["query_type"] = packet[DNSQR].qtype
+        
+        dns_queries.append(dns_query)
 
-dns_quries = []
-
-headers = ['time', 'b', 'c', 'd', 'e']
-
-data_csv = open('data1.csv', 'w')
-c = csv.DictWriter(data_csv, fieldnames=headers)
-c.writerows(dns_quries)
-data_csv.close()
-
-
-sniff(filter="udp and port 53",prn=process_pack, count=0)  
+dns_queries = []
+sniff(filter="udp and port 53",prn=process_pack, count=60)  
+   
+def save_to_json():
+     with open('data.json', 'w') as json_file:
+        json.dump(dns_queries, json_file, indent=4) 
+save_to_json()
 
 
 
