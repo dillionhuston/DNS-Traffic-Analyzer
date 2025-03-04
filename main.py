9ab22ba12f75d68683dd28ea0faad26b78b4c476
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-03-04 16:33:00 +0000

    Better readability

--- main.py
+++ main.py
@@ -18,23 +18,21 @@ def process_pack(packet):
             'timestamp': packet.time,            
             'dst_ip': packet[1].dst,             
             'query_name': packet[DNSQR].qname.decode(),  
-            'query_type': packet[DNSQR].qtype  
-
+            'query_type': packet[DNSQR].qtype 
     })
-        
     else:
         query = False
 
         
 def save_data(dns_queries, query):
-        
-        df = pd.DataFrame(dns_queries)
-        with pd.ExcelWriter(dns_traffic_data, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
-            df.to_excel(writer, index=False, header='data')
-            if writer:
-                print("data saved")
 
-            query = False
+    df = pd.DataFrame(dns_queries)
+    with pd.ExcelWriter(dns_traffic_data, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+     df.to_excel(writer, index=False, header='data')
+
+    if writer:
+        print("data saved")
+        query = False
             
 
 
