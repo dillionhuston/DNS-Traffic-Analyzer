ba9916f4addd9fe16849d501eca41891179cdccf
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-03-04 16:14:05 +0000

    Added excel sheet functionaly

--- main.py
+++ main.py
@@ -5,44 +5,43 @@ import json
 import pickle
 import csv
 from openpyxl import Workbook as op
-from handler import *
+import handler 
 
 
-dns_traffic_data = r"C:\Users\dillionn\Downloads\data.xlsx"
+dns_traffic_data = r"C:\Users\\Desktop\Projects\Network_monitor\Netwrok-Traffic-Analyser\data.xlsx"
 dns_queries = []
-
 
 def process_pack(packet):
     if packet.haslayer(DNS) and packet.haslayer(DNSQR):
-
         query = True
         dns_queries.append({
             'timestamp': packet.time,            
             'dst_ip': packet[1].dst,             
             'query_name': packet[DNSQR].qname.decode(),  
-            'query_type': packet[DNSQR].qtype   
-        })
-        sniff(filter="udp and port 53",prn=process_pack, count=60)
+            'query_type': packet[DNSQR].qtype  
 
+    })
+        
+    else:
+        query = False
 
-def save_data(dns_queries, dns_query, query):
+        
+def save_data(dns_queries, query):
         
         df = pd.DataFrame(dns_queries)
-        with pd.ExcelWriter(dns_traffic_data, mode='a', engine='openpyxl') as writer:
-            df.to_excel(writer, index=False, header=not writer.sheets)
+        with pd.ExcelWriter(dns_traffic_data, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+            df.to_excel(writer, index=False, header='data')
+            if writer:
+                print("data saved")
+
+            query = False
+            
 
 
-            print("data saved")
+sniff(filter="udp and port 53",prn=process_pack, count=60)
+save_data(dns_queries)
 
 
-
-def test(dns_qureies):
-     print(dns_queries[0])
-
-#sniff(filter="udp and port 53", prn=process_pack, count=60)
-#save_data()
-
-test()
 
         
 
