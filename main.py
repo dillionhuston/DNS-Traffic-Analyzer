23e49f6dc6c511881342b6bb8cf0a4e80948e72e
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-03-04 21:23:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from scapy.all import sniff, DNS, DNSQR, packet
 import pandas as pd
-import numpy as np 
-import json
-import pickle
-import csv
+from scapy.all import DNS, DNSQR
+from handler import HandleData
 from openpyxl import Workbook as op
-import handler 
+from openpyxl import load_workbook
 
 
-dns_traffic_data = r"C:\Users\\Desktop\Projects\Network_monitor\Netwrok-Traffic-Analyser\data.xlsx"
+dns_traffic_data = r"data.xlsx"
 dns_queries = []
 
-def process_pack(packet):
+def process_pack(packet, load_workbook):
     if packet.haslayer(DNS) and packet.haslayer(DNSQR):
         query = True
         dns_queries.append({
@@ -23,26 +20,10 @@ def process_pack(packet):
     else:
         query = False
 
-        
-def save_data(dns_queries, query):
-
+def save_data(dns_queries):
     df = pd.DataFrame(dns_queries)
     with pd.ExcelWriter(dns_traffic_data, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
-     df.to_excel(writer, index=False, header='data')
+        df.to_excel(writer, index=False, header=True)  
+   
 
-    if writer:
-        print("data saved")
-        query = False
-            
-
-
-sniff(filter="udp and port 53",prn=process_pack, count=60)
-save_data(dns_queries)
-
-
-
-        
-
-    
-
-
+HandleData.read_data(dns_traffic_data) 
